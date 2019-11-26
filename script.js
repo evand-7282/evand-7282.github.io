d790a1797580feaee066781cf029ff5cc2a5d9f5
Evan Dono <evand7282@hstat.org>
2019-11-26 15:26:24 +0000

    organize repo's and fix issues

--- script.js
+++
@@ -1,9 +0,0 @@
-let ownerName = "Evan Dono"
-let userName = "evand-7282";
-
-document.querySelectorAll(".owner-name").forEach((e) => {
-    e.innerHTML = ownerName;
-});
-
-document.querySelector("#github").href = "http://github.com/" + userName;
-document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
